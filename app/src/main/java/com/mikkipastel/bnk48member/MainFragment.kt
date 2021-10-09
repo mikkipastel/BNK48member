e9ce1b1ad225adbe8c83e7e8806d1bc6d1d2aacc
Donyawan Kritaisong <donyawan.kri@cpmatch.com>
2021-10-10 00:43:28 +0700

    - added viewBinding

--- app/src/main/java/com/mikkipastel/bnk48member/MainFragment.kt
+++ app/src/main/java/com/mikkipastel/bnk48member/MainFragment.kt
@@ -10,14 +10,18 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.mikkipastel.bnk48member.adapter.MemberAdapter
 import com.mikkipastel.bnk48member.adapter.MemberListener
+import com.mikkipastel.bnk48member.databinding.FragmentMainBinding
 import com.mikkipastel.bnk48member.model.ListMemberItem
 import com.mikkipastel.bnk48member.model.Member
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment: Fragment(), MemberListener {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(layoutInflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,7 +31,7 @@ class MainFragment: Fragment(), MemberListener {
 
         val memberAdapter = MemberAdapter(memberList, this)
 
-        recyclerView.apply {
+        binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
             isNestedScrollingEnabled = false
             adapter = memberAdapter
